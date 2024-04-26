6190f4ddf623ddd5b4de299b0593c39b4a552c11
anandmish123ra <168157278+anandmish123ra@users.noreply.github.com>
2024-04-26 20:00:24 +0530

    My first github  webpage

---
+++ age.js
@@ -0,0 +1,33 @@
+let inputBtn=document.querySelector(".age-dob-input");
+let btn=document.querySelector(".btn");
+let ageResult=document.querySelector(".result");
+
+btn.addEventListener("click",()=>{
+    if(inputBtn.value===""){
+        alert("please select your date of birth");
+    }
+        else{
+            console.log(inputBtn.value);
+            let dob=new Date(inputBtn.value);
+            console.log(dob);
+
+            let dob_year=dob.getFullYear();
+            console.log(dob_year);
+
+            //current
+            let now=new Date();
+            console.log(now);
+            let now_year=now.getFullYear();
+            console.log(now_year);
+
+            //final ans
+            let age=now_year-dob_year;
+            console.log(age);
+
+            //result
+ageResult.textContent=` your age is : ${age}`;
+        }
+
+        
+    
+})
